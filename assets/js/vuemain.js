d8c0a64cec6e70dc9cb60a4d8f1574e1fd549bcf
Kalyan Vishnubhatla <kalyan@Kalyans-MacBook-Pro.local>
2017-11-11 20:26:53 -0800

    Initial code

--- assets/js/vuemain.js
+++ assets/js/vuemain.js
@@ -1,14 +1,20 @@
 new Vue({
   el: '#app',
   data: {
-    title: "Hello World!",
-    link: "http://www.google.com",
-    finishedLink: "<a href='http://google.com'>Google</a>"
+    counter: 0,
+    x: 0,
+    y: 0
   },
   methods: {
-    sayHello: function() {
-      this.title = "Hello";
-      return this.title;
+    increase: function(amount, event) {
+      this.counter = this.counter + amount;
+    },
+    updatecoordinates: function(event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
+    },
+    alertMe: function() {
+      alert('alert');
     }
   }
 });
